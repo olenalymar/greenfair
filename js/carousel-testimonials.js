28677554a757a9197df7c0092d743bfe6fed1a28
helena <32456911+olenalymar@users.noreply.github.com>
2017-11-01 10:52:58 +0200

    Add files via upload

---
+++ js/carousel-testimonials.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.carousel-testimonials').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 2,
+		slidesToScroll: 2
+      });
+    });
---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    infinite: true,
+    speed: 500
+  });
+});
